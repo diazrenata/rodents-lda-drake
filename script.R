429cf1cab3ddf69c5c51a5c43cc6b638f0cc4bb3
Renata Diaz <diaz.renatam@gmail.com>
2019-08-13 07:25:48 -0400

    cleanup

--- script.R
+++ script.R
@@ -4,28 +4,29 @@ library(MATSS)
 library(LDATS)
 
 pipeline <- drake_plan(
-
+  
   portal_data = MATSS::get_portal_rodents(),
   
   portal_lda = LDATS::LDA_set(document_term_table = portal_data$abundance,
-                                         topics = c(2:6),
-                                         nseeds = 10,
-                                         control = LDA_controls_list(quiet = TRUE)),
+                              topics = c(2:6),
+                              nseeds = 10,
+                              control = LDA_controls_list(quiet = TRUE)),
+  
+  
   
   portal_lda_selected = LDATS::select_LDA(portal_lda),
   
   portal_changepoint_models = TS_on_LDA(LDA_models = portal_lda_selected, 
-                                  document_covariate_table = portal_data$covariates,
-                                  formulas = ~ newmoonnumber,
-                                  nchangepoints = c(0:6), 
-                                  weights = document_weights(portal_data$abundance),
-                                  control = TS_controls_list(nit = 100, timename = "newmoonnumber")), 
+                                        document_covariate_table = portal_data$covariates,
+                                        formulas = ~ newmoonnumber,
+                                        nchangepoints = c(0:6), 
+                                        weights = document_weights(portal_data$abundance),
+                                        control = TS_controls_list(nit = 100, timename = "newmoonnumber")), 
   
   portal_cpt_selected = select_TS(portal_changepoint_models)
   
-  )
-  
-print(portal_cpt_selected$nchangepoints)
+)
+
 
 db <- DBI::dbConnect(RSQLite::SQLite(), here::here("drake", "drake-cache.sqlite"))
 cache <- storr::storr_dbi("datatable", "keystable", db)
